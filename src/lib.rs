ae88e9f0d50f575029474e636bcab42bc5ace4a8
allthemusicllc <make.allthemusic@protonmail.com>
2020-02-26 23:09:18 -0600

    Updated documentation to remove reference to public domain

--- src/lib.rs
+++ src/lib.rs
@@ -8,8 +8,8 @@
 
 //! `atm-cli` is a command line tool for generating and working with MIDI files. It was purpose-built for
 //! All the Music, LLC to assist in its mission to enable musicians to make all of their music
-//! without the fear of frivolous copyright lawsuits. All code is released into the public domain
-//! via the [Creative Commons Attribute 4.0 International License](http://creativecommons.org/licenses/by/4.0/).
+//! without the fear of frivolous copyright lawsuits. All code is freely available under the
+//! [Creative Commons Attribute 4.0 International License](http://creativecommons.org/licenses/by/4.0/).
 //! If you're looking for a Rust library to generate and work with MIDI files, check out
 //! [the `libatm` project](https://github.com/allthemusicllc/libatm), on which this tool relies. For
 //! more information on All the Music, check out [allthemusic.info](http://allthemusic.info).
