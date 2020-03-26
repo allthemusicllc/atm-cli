03a6c036de6305c21e8cc3f295a4688686cc9c1e
allthemusicllc <make.allthemusic@protonmail.com>
2020-03-26 02:06:54 -0500

    Updated main to use proper CLI api

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,11 @@
 // To view a copy of this license, visit http://creativecommons.org/licenses/by/4.0/ or send
 // a letter to Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
 
+use atm::cli::CliDirective;
+
+use structopt::StructOpt;
+
 fn main() {
     // Parse command line arguments and run program
-    atm::cli::Cli::new().run();
+    atm::cli::Cli::from_args().run();
 }
